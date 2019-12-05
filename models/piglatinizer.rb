61505078b22fb8837de1fabf8a68cd52cbe7d648
Jared S <jaredjames2020@gmail.com>
2019-12-05 22:51:15 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,10 +4,10 @@ class PigLatinizer
     vowels = %w{A E I O U a e i o u}
     word.each_char do |chr|
     index = word.index(chr)
-    if index != 0 && vowels.include?(chr.downcase)
+    if index != 0 && vowels.include?(chr)
       consonants = word.slice!(0..index-1)
       return word + consonants + "ay"
-    elsif index == 0 && vowels.include?(chr.downcase)
+    elsif index == 0 && vowels.include?(chr)
       return word + "ay"
     end
   end
