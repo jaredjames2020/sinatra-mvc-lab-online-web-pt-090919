0a50d98b47c2d9aad273148c8cf2ec5529625eac
Jared S <jaredjames2020@gmail.com>
2019-12-05 22:51:47 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,6 +2,17 @@ class PigLatinizer
   
   def piglatinize(word)
     word[0] =~ /[AEIOUaeiou]/ ? word.gsub(/([AEIOUaeiou])(\w+)*/, '\1\2way') : word.gsub(/([^AEIOUaeiou])(\w+)*/, '\2\1ay')
+    
+        vowels = %w{a e i o u}
+    word.each_char do |chr|
+    index = word.index(chr)
+    if index != 0 && vowels.include?(chr.downcase)
+      consonants = word.slice!(0..index-1)
+      return word + consonants + "ay"
+    elsif index == 0 && vowels.include?(chr.downcase)
+      return word + "ay"
+    end
+  end
   end
   
   
